d56fa1bb7a6381dfe6728a6ae8873066e0c1c202
Morgan Schwartz <msschwartz21@gmail.com>
2025-04-28 11:48:14 -0400

    Drop spatial graph support and move development to dev-sg branch (#40)
    * Drop spatial graph support and move development to dev-sg branch
    
    * Make networkx a required, not optional, dependency

--- src/geff/spatial_graph/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    import spatial_graph
-except ImportError as err:
-    raise ImportError(
-        "The spatial_graph submodule depends on spatial-graph as an optional dependency. "
-        "Please run `pip install geff[spatial-graph]` to install the optional dependency."
-    ) from err
-
-from .io import read, write
--- tests/test_spatial_graph/test_overwrites.py
+++
@@ -1,6 +0,0 @@
-# to test:
-
-# path/to.zarr/group
-# path/to.zarr
-
-# make sure we only overwrite what we are allowed to
