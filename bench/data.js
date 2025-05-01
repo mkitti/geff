89cc2e7ff1ddff8296a98c9b7687a0330466bba2
github-action-benchmark <github@users.noreply.github.com>
2025-05-01 20:40:40 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 8018bf0741cf2905d028c1e9876921a6a766d230

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746129266789,
+  "lastUpdate": 1746132040555,
   "repoUrl": "https://github.com/live-image-tracking-tools/geff",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.5470120897994015",
             "extra": "mean: 15.872789203666656 sec\nrounds: 3"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "malinmayorc@janelia.hhmi.org",
+            "name": "Caroline Malin-Mayor",
+            "username": "cmalinmayor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8018bf0741cf2905d028c1e9876921a6a766d230",
+          "message": "Merge pull request #43 from live-image-tracking-tools/41-metadata\n\nSave and load metadata fully",
+          "timestamp": "2025-05-01T16:38:05-04:00",
+          "tree_id": "c4b3156aeccc9b46afccf0eb4eea4417cc6807ea",
+          "url": "https://github.com/live-image-tracking-tools/geff/commit/8018bf0741cf2905d028c1e9876921a6a766d230"
+        },
+        "date": 1746132040086,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/bench.py::test_write",
+            "value": 0.13512345883098548,
+            "unit": "iter/sec",
+            "range": "stddev: 0.02128361089617375",
+            "extra": "mean: 7.40063944966666 sec\nrounds: 3"
+          },
+          {
+            "name": "tests/bench.py::test_validate",
+            "value": 365.99921017248704,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00029044500712752845",
+            "extra": "mean: 2.7322463333424216 msec\nrounds: 3"
+          },
+          {
+            "name": "tests/bench.py::test_read",
+            "value": 0.06277695766855433,
+            "unit": "iter/sec",
+            "range": "stddev: 0.5359005540661197",
+            "extra": "mean: 15.929411636666666 sec\nrounds: 3"
           }
         ]
       }
