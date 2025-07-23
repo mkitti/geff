6fe8929a2841ca299e82e5aeaba99f14d123fb38
Laura Xénard <33941387+lxenard@users.noreply.github.com>
2025-07-23 16:08:18 -0400

    TrackMate XML to GEFF (#197)
    # Proposed Change
    Add a converter to convert TrackMate XML to GEFF.
    For now, it only extracts the graph data, including nodes and edges
    properties, and relies on write_nx to build a valid GEFF.
    Relates to #69 .
    
    # Types of Changes
    What types of changes does your code introduce? Delete those that do not
    apply.
    - New feature or enhancement
    - Tests
    
    Which topics does your change affect? Delete those that do not apply.
    - io
    
    # Checklist
    Put an x in the boxes that apply. You can also fill these out after
    creating the PR. If you're unsure about any of them, don't hesitate to
    ask. We're here to help! This is simply a reminder of what we are going
    to look for before merging your code.
    
    - [x] I have read the
    [developer/contributing](https://github.com/live-image-tracking-tools/geff/blob/main/CONTRIBUTING)
    docs.
    - [x] I have added tests that prove that my feature works in various
    situations or tests the bugfix (if appropriate).
    - [ ] I have checked that I maintained or improved code coverage.
    - [ ] I have written docstrings and checked that they render correctly
    by looking at the docs preview (link left as a comment on the PR).
    
    # Further Comments
    The actual implementation relies on iterparse from lxml (not part of
    standard library). It means that the converter iteratively parses the
    XML, line by line. At no point is the XML completely loaded into memory.
    So, nice for crazy huge XMLs but probably comes at the cost of
    efficiency.

--- src/geff/utils.py
+++ src/geff/utils.py
@@ -4,6 +4,8 @@ import os
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+import networkx as nx
+import networkx.algorithms.isomorphism as iso
 import zarr
 
 if TYPE_CHECKING:
@@ -114,3 +116,60 @@ def validate(store: StoreLike):
                         f"Edge property {prop} missing mask has length {missing_len}, "
                         f"which does not match id length {edge_id_len}"
                     )
+
+
+def nx_is_equal(g1: nx.Graph, g2: nx.Graph) -> bool:
+    """Utility function to check that two Network graphs are perfectly identical.
+
+    It checks that the graphs are isomorphic, and that their graph,
+    nodes and edges attributes are all identical.
+
+    Args:
+        g1 (nx.Graph): The first graph to compare.
+        g2 (nx.Graph): The second graph to compare.
+
+    Returns:
+        bool: True if the graphs are identical, False otherwise.
+    """
+    edges_attr = list({k for (n1, n2, d) in g2.edges.data() for k in d})
+    edges_default = len(edges_attr) * [0]
+    em = iso.categorical_edge_match(edges_attr, edges_default)
+    nodes_attr = list({k for (n, d) in g2.nodes.data() for k in d})
+    nodes_default = len(nodes_attr) * [0]
+    nm = iso.categorical_node_match(nodes_attr, nodes_default)
+
+    if not g1.nodes.data() and not g2.nodes.data():
+        same_nodes = True
+    elif len(g1.nodes.data()) != len(g2.nodes.data()):
+        same_nodes = False
+    else:
+        for data1, data2 in zip(sorted(g1.nodes.data()), sorted(g2.nodes.data()), strict=False):
+            n1, attr1 = data1
+            n2, attr2 = data2
+            if sorted(attr1) == sorted(attr2) and n1 == n2:
+                same_nodes = True
+            else:
+                same_nodes = False
+
+    if not g1.edges.data() and not g2.edges.data():
+        same_edges = True
+    elif len(g1.edges.data()) != len(g2.edges.data()):
+        same_edges = False
+    else:
+        for data1, data2 in zip(sorted(g1.edges.data()), sorted(g2.edges.data()), strict=False):
+            n11, n12, attr1 = data1
+            n21, n22, attr2 = data2
+            if sorted(attr1) == sorted(attr2) and sorted((n11, n12)) == sorted((n21, n22)):
+                same_edges = True
+            else:
+                same_edges = False
+
+    if (
+        nx.is_isomorphic(g1, g2, edge_match=em, node_match=nm)
+        and g1.graph == g2.graph
+        and same_nodes
+        and same_edges
+    ):
+        return True
+    else:
+        return False
